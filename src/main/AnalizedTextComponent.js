9a5bbaef48f9d9957e2b1b2b5559995b46dda30e
Konstantin Pentchev <kpentchev@Admin-01Nov18s-MacBook-Pro.local>
2019-03-18 14:03:03 +0100

    add popover

--- src/main/AnalizedTextComponent.js
+++ src/main/AnalizedTextComponent.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
+import { Popover } from 'antd';
+import 'antd/lib/popover/style/css';
+
+const type2label = {
+    gam: 'game',
+    org: 'organization',
+    per: 'person',
+    mon: 'money',
+    geo: 'geolocation',
+    tou: 'tournament',
+    tim: 'time',
+    tea: 'team'
+}
+
 const processText = (text, predictions) => {
     if (!text || !predictions)
         return
     const result = [];
     let lastIdx = 0;
     predictions.forEach(prediciton => {
+        const ner = text.substring(prediciton.start, prediciton.end)
         result.push(text.substring(lastIdx, prediciton.start))
-        result.push(<mark className={prediciton.type}>{text.substring(prediciton.start, prediciton.end)}</mark>)
+        result.push(<Popover content={ner} title={type2label[prediciton.type]}><mark className={prediciton.type}>{ner}</mark></Popover>)
         lastIdx = prediciton.end
     });
     result.push(text.substring(lastIdx));
