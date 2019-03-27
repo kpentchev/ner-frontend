3b9bdd1957fb162e0fcaf573dd568cecc1f2e453
Konstantin Pentchev <kpentchev@Admin-01Nov18s-MacBook-Pro.local>
2019-03-27 16:15:33 +0100

    render metadata in popover

--- src/main/AnalizedTextComponent.js
+++ src/main/AnalizedTextComponent.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import { Popover } from 'antd';
+import { Popover, List, Tag } from 'antd';
 import 'antd/lib/popover/style/css';
+import 'antd/lib/list/style/css';
+import 'antd/lib/tag/style/css';
 
 const type2label = {
     gam: 'game',
@@ -21,8 +23,9 @@ const processText = (text, predictions) => {
     let lastIdx = 0;
     predictions.forEach((prediction, idx) => {
         const ner = text.substring(prediction.start, prediction.end)
+        const meta = prediction.meta
         result.push(text.substring(lastIdx, prediction.start))
-        result.push(<Popover key={`prediction-${idx}`} content={ner} title={type2label[prediction.type]}><mark className={prediction.type}>{ner}</mark></Popover>)
+        result.push(<Popover key={`prediction-${idx}`} content={<PopoverContent text={ner} meta={meta} />} title={type2label[prediction.type]}><mark className={prediction.type}>{ner}</mark></Popover>)
         lastIdx = prediction.end
     });
     result.push(text.substring(lastIdx));
@@ -31,5 +34,28 @@ const processText = (text, predictions) => {
 
 const AnalyzedTextComponent = ({text, predictions}) =>
     <p className="text-predicitons" children={processText(text, predictions)} />
+
+const PopoverContent = ({text, meta}) =>
+    <div>
+        <List
+            size="small"
+            bordered
+            dataSource={Object.keys(meta)}
+            renderItem={key => 
+                <List.Item>
+                    <List.Item.Meta title={<h4>{key}</h4>} />
+                    <MetaValue value={meta[key]} />
+                </List.Item>
+            }
+        />
+    </div>
+
+const MetaValue = ({value}) =>
+    Array.isArray(value) ? 
+    <List
+        size="small"
+        dataSource={value}
+        renderItem={item =>  <List.Item>{item}</List.Item>}
+    /> : <span>{value}</span>
 
 export default AnalyzedTextComponent;
